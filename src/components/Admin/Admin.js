3f7b33f8730de361ed0bb30a23e4306bc571ad5c
Maxwell Todd <mtmaxtodd@gmail.com>
2019-02-01 16:40:04 -0600

    Added and imported components necessary for the the two pages of the app. Added dependencies to package.json

---
+++ src/components/Admin/Admin.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import SearchResultsItem from './SearchResultsItem';
+
+class SearchResults extends Component {
+
+    appendToDOM = () => {
+
+
+        return this.props.reduxStore.searchList.map((result) => {
+            return <SearchResultsItem key={result.id} result={result} />
+        })
+
+    }
+
+    render() {
+        return (
+            <div>
+                {this.appendToDOM()}
+            </div>
+        );
+    }
+}
+
+const mapStoreToProps = reduxStore => ({
+    reduxStore,
+})
+
+export default connect(mapStoreToProps)(SearchResults);
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import Admin from '../Admin/Admin.js';
+import Project from '../Project/Project.js';
 class App extends Component {
   // Renders the entire app on the DOM
   render() {
