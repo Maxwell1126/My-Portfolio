6c2cc6a2cfe14db2f297f0baba032557c6e4571a
Maxwell Todd <mtmaxtodd@gmail.com>
2019-02-03 21:25:32 -0600

    Sharpened styling.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,7 +4,6 @@ import { HashRouter as Router, Route,} from 'react-router-dom';
 import Admin from '../Admin/Admin.js';
 import Project from '../Project/Project.js';
 import 'typeface-roboto';
-import Typography from '@material-ui/core/Typography';
 class App extends Component {
   // Renders the entire app on the DOM
   render() {
