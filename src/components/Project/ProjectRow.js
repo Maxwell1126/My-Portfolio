fc0ff5a20542306a49ec690bdab13db0cd80decc
Maxwell Todd <mtmaxtodd@gmail.com>
2019-02-03 18:06:37 -0600

    sharpened the style of the project page.

--- src/components/Project/ProjectRow.js
+++ src/components/Project/ProjectRow.js
@@ -5,46 +5,53 @@ import MenuItem from '@material-ui/core/MenuItem';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import CardContent from '@material-ui/core/CardContent';
+import 'typeface-roboto';
+import Typography from '@material-ui/core/Typography';
 import './Project.css';
 class ProjectRow extends Component {
     render() {
         let appendName = null;
         if (this.props.project.name !== null) {
-            appendName = <p>{'Name: ' + this.props.project.name}</p>
+            appendName = <p><strong><u>Name:  </u></strong>{this.props.project.name}</p>
         }
         let appendDescription = null;
         if (this.props.project.description !== null) {
-            appendDescription = <p>{'Description: ' + this.props.project.description}</p>
+            appendDescription = <p><strong><u>Description:  </u></strong>{this.props.project.description}</p>
         }
         let appendThumbnail = null;
         if (this.props.project.thumbnail !== null) {
-            appendThumbnail = <p>{'Thumbnail: ' + this.props.project.thumbnail}</p>
+            appendThumbnail = <p><strong><u>Thumbnail:  </u></strong>{this.props.project.thumbnail}</p>
         }
         let appendWebsite = null;
         if (this.props.project.website !== null) {
-            appendWebsite = <p>{'Website: '}{<a href={this.props.project.website}>Website</a>}</p>
+            appendWebsite = <p><strong><u>Website:  </u></strong>{<a href={this.props.project.website}>Website</a>}</p>
         }
         let appendGithub = null;
         if (this.props.project.github !== null) {
-            appendGithub = <p>{'GitHub: '}{<a href={this.props.project.github}>GitHub</a>}</p>
+            appendGithub = <p><strong><u>GitHub:  </u></strong>{<a href={this.props.project.github}>GitHub</a>}</p>
         }
         let appendDate = null;
         if (this.props.project.date_completed !== null) {
-            appendDate = <p>{'Date: ' + this.props.project.date_completed.substr(0,10)}</p>
+            appendDate = <p><strong><u>Date: </u></strong>{this.props.project.date_completed.substr(0,10)}</p>
         }
         return (
             <Card className="card">
+                <Typography>
                 <CardContent className="content">
+                
                     {appendName}
                     {appendDate}
                     {appendThumbnail}
                     {appendWebsite}
                     {appendGithub}
-                    <p>{'Tag: ' + this.props.project.tag_name}</p>
+                        <p><strong><u>Tag:  </u></strong> {this.props.project.tag_name}</p>
                 </CardContent>
                 <CardContent >
+
                     {appendDescription}
+                    
                 </CardContent> 
+                </Typography>
             </Card>)
     }
 }
